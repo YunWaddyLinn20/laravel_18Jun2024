2d644ba92ff5045c0f3fe1b14d3e217055a7bd22
LoryLinn235 <loryestellelinn23@gmail.com>
2024-06-20 08:35:41 +0630

    project start

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [App\Http\Controllers\FrontendController::class, 'index'])->name('front.index');
+Route::get('item/{item_id}', [App\Http\Controllers\FrontendController::class, 'show'])->name('front.show');
+Route::group(['prefix'=>'backend', 'as'=>'backend.'],function(){
+    Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+
 });
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
